6299cade9e4b751be30cb54b686e1e0f3a5a034c
Matthias Wiesner <mw@cloudcontrol.de>
2013-11-05 12:08:22 +0100

    Add dynamic weeks

--- emergencycontrol/controller.py
+++ emergencycontrol/controller.py
@@ -1,9 +1,9 @@
 from emergencycontrol import app
 from flask import request, render_template, redirect, url_for, flash
-from .model import User, Person, db_session, EmergencyService
+from .model import Person, db_session, EmergencyService
 from .forms import PersonForm
 from flask.ext.login import login_required
-from datetime import datetime
+from datetime import date, datetime, timedelta
 
 
 @app.route('/person', methods=['GET', 'POST'])
@@ -48,6 +48,52 @@ def swap():
     return ''
 
 
+@app.route('/set', methods=['POST'])
+@login_required
+def set():
+    week_id = int(request.form['week_id'])
+    week = EmergencyService.query.get(week_id)
+
+    week.person_id = int(request.form['person_id'])
+
+    db_session.add(week)
+    db_session.commit()
+
+    return ''
+
+
+@app.route('/calendar')
+def calendar():
+    now = date.today()
+    weeks = EmergencyService.query.order_by(EmergencyService.start_date.asc()).all()
+    weeks_from_today = [week for week in weeks if week.end_date >= now]
+    
+    c = len(weeks_from_today)
+    if c < 12:
+        last_week = weeks_from_today[c-1]
+        current_year = last_week.end_date.year
+        current_week = last_week.week_nr
+        new_weeks = 0
+        while new_weeks < 12-c:
+            current_week += 1
+            start_date = datetime.strptime('{0} {1} 1'.format(current_year, current_week-1), '%Y %W %w')
+            end_date = start_date + timedelta(days=7, seconds=-1)
+            current_week = start_date.isocalendar()[1]
+            current_year = start_date.isocalendar()[0]
+            es = EmergencyService(week_nr=current_week, start_date=start_date, end_date=end_date)
+            db_session.add(es)
+            new_weeks += 1
+        db_session.commit()
+
+    persons = Person.query.all()
+    for week in weeks:
+        for p in persons:
+            if p.id == week.person_id:
+                week.person = p
+    return render_template('calendar.jinja',weeks=weeks_from_today,persons=persons)
+
+
+
 @app.route('/reset', methods=['GET', 'POST'])
 @login_required
 def reset():
@@ -57,16 +103,7 @@ def reset():
     return redirect(url_for('person'))
 
 
-@app.route('/testcall')
-def testcall():
-    return request.args.get('emps')
-
-@app.route('/calendar')
-def calendar():
-    current_week = datetime.now().isocalendar()[1]
-    weeks = EmergencyService.query.all()
-    weeks_from_today = [week for week in weeks if week.week_nr >= current_week]
-    persons = Person.query.all()
-    for week in weeks:
-        week.person = Person.query.get(week.person_id)
-    return render_template('calendar.jinja',weeks=weeks_from_today,persons=persons)
+@app.route('/migrate')
+def migrate():
+    # do what you want
+    return ''
--- emergencycontrol/static/js/emergency.js
+++ emergencycontrol/static/js/emergency.js
@@ -1,22 +1,34 @@
 $(".draggable").draggable({
 	helper : "clone",
-	cursor: "move"
+	cursor: "move",
+	activeClass : "draggable-hover",
 });
 
 $(".droppable").droppable({
-	activeClass : "ui-state-default",
-	hoverClass : "ui-state-hover",
-	accept : ".droppable",
-	drop : function(event, ui) {
+	hoverClass : "droppable-hover",
+	accept : ".draggable",
+	drop : function(event, ui) {	
 		var nameFrom = ui.draggable.text();
 		var nameTo = $(this).text();
-		
-		$(this).text(nameFrom);
-		ui.draggable.text(nameTo);
-		
-		$.post("./swap", {
-			week_from_id : ui.draggable.attr("data-week-id"),
-			week_to_id : $(this).attr("data-week-id")
-		});
+
+		if (ui.draggable.attr("data-week-id") != undefined) {
+			$(this).text(nameFrom);
+			ui.draggable.text(nameTo);
+			console.log("SWAP")
+			$.post("./swap", {
+				week_from_id : ui.draggable.attr("data-week-id"),
+				week_to_id : $(this).attr("data-week-id")
+			});
+
+		} else if(ui.draggable.attr("data-person-id") != undefined) {
+			$(this).text(nameFrom);
+			$(this).removeClass("notassigned");
+			
+			console.log("SET")
+			$.post("./set", {
+				person_id : ui.draggable.attr("data-person-id"),
+				week_id : $(this).attr("data-week-id")
+			});
+		} 
 	}
 });
