b232f483bfaf25a319e3f3c4cc650aaa352f5b76
Matthias Wiesner <mw@cloudcontrol.de>
2014-03-03 12:16:13 +0100

    Simplify tasks fetching and rendering

--- emergencycontrol/static/js/tasks.js
+++ emergencycontrol/static/js/tasks.js
@@ -1,35 +1,4 @@
-$(function () {
-    $('#container').highcharts({
-        chart: {
-            type: 'spline'
-        },
-        title: {
-            text: 'Tasks'
-        },
-        xAxis: {
-            type: 'datetime'
-        },
-        yAxis: {
-            title: {
-                text: null,
-                enabled: false
-            },
-            min: 0
-        },
-        plotOptions: {
-            series: {
-                marker: {
-                    enabled: false
-                }
-            }
-        },
-        series: [{
-            name: 'task open',
-            data: taskOpen
-
-        }, {
-            name: 'task executed',
-            data: taskExecuted
-        }]
-    });
+$.getJSON("/tasks", function( config ) {
+    config['chart']['backgroundColor'] = 'white';
+    new Highcharts.Chart(config);
 });
--- emergencycontrol/tasks.py
+++ emergencycontrol/tasks.py
@@ -5,31 +5,13 @@ from flask_login import login_required
 from flask import render_template
 
 
-def fetch_data():
-    content = urllib2.urlopen("https://kpi.cloudcontrolled.com/kpi/tasks").read()
-    data = json.loads(content)
-    task_open = None
-    task_executed = None
-    for task_data in data['series']:
-        if task_data['name'] == 'task_open':
-            task_open = task_data['data']
-        if task_data['name'] == 'task_executed':
-            task_executed = task_data['data']
-    return task_open, task_executed
-
-
 @app.route('/showtasks')
 @login_required
 def showtasks():
-    task_open, task_executed = fetch_data()
-    return render_template('tasks.jinja', task_open=json.dumps(task_open), task_executed=json.dumps(task_executed))
+    return render_template('tasks.jinja')
 
 
 @app.route('/tasks')
 @login_required
 def tasks():
-    task_open, task_executed = fetch_data()
-    return json.dumps({
-        'taskOpen': task_open,
-        'taskExecuted': task_executed
-    })
+    return urllib2.urlopen("https://kpi.cloudcontrolled.com/kpi/tasks").read()
